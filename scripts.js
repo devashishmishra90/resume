b6ddb2ca98950853db733b31257ed9cb60f52c9a
devashishmishra <devashish.mishra@nagarro.com>
2024-11-03 11:31:13 +0530

    files added

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to display a greeting message
+function showGreeting() {
+  alert("Hello! Welcome to my portfolio!");
+}
+
+// Adding event listener to the button
+document.getElementById("greetButton").addEventListener("click", showGreeting);
